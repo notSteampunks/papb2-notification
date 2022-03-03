6352fea411ccf05e9ae416a9f7ed3266920f0ac2
notSteampunks <isnanda.a@mail.ugm.ac.id>
2022-03-03 14:31:47 +0700

    add intent

--- app/src/main/java/com/example/notificationproject/MainActivity.java
+++ app/src/main/java/com/example/notificationproject/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.core.os.BuildCompat;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -65,10 +67,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void sendNotification(){
+        Intent contentIntent = new Intent(getApplicationContext(), MainActivity.class);
+        PendingIntent pendingContentIntent = PendingIntent.getActivity(getApplicationContext(),NOTIF_ID, contentIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+
         NotificationCompat.Builder built = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID);
         built.setContentTitle("You've been notified");
         built.setContentText("This is notification text");
         built.setSmallIcon(R.drawable.ic_notification);
+        built.setContentIntent(pendingContentIntent);
 
         Notification notif = built.build();
         mNotificationManager.notify(NOTIF_ID, notif);
